b87d5a645a237e99b857bc619c5c036c965a636f
Nikita Pot <nikita.pot@d2t.nl>
2024-02-15 13:04:47 +0100

    emoji dictionary

--- app/src/main/java/com/nikitapot/emoji_dictionary/EmojiAdapter.kt
+++ app/src/main/java/com/nikitapot/emoji_dictionary/EmojiAdapter.kt
@@ -9,11 +9,13 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 
 
-
 class EmojiAdapter(val emoji: ArrayList<String>) : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
-        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row,parent,false))
+        return TextHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item_row, parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
@@ -26,21 +28,23 @@ class EmojiAdapter(val emoji: ArrayList<String>) : RecyclerView.Adapter<EmojiAda
     }
 
 
-    class TextHolder(v: View) : RecyclerView.ViewHolder(v),View.OnClickListener{
-       var view: View = v
+    class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        var view: View = v
         var emoji: String = ""
-            init {
-                v.setOnClickListener(this)
-            }
 
-        fun bindEmoji(emoji: String){
-           this.emoji = emoji
+        init {
+            v.setOnClickListener(this)
+        }
+
+        fun bindEmoji(emoji: String) {
+            this.emoji = emoji
             view.findViewById<TextView>(R.id.itemTextView).text = emoji
         }
+
         override fun onClick(v: View?) {
-            val detailIntent  = Intent(view.context,EmojiDetailActivity::class.java)
-            detailIntent.putExtra("emoji",emoji)
-            startActivity(view.context,detailIntent,null)
+            val detailIntent = Intent(view.context, EmojiDetailActivity::class.java)
+            detailIntent.putExtra("emoji", emoji)
+            startActivity(view.context, detailIntent, null)
         }
 
     }
